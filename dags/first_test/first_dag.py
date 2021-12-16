d760b960b3371822eb7c1307098c2e2f8823b8e0
unknown <arthurcapacitacion@gmail.com>
2021-12-16 11:13:55 -0400

    create first test dag

---
+++ dags/first_test/first_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+pipeline = DAG(
+    'first_dag',
+    'First Pipeline as DAG',
+    start_date=datetime.today() - timedelta(days=1),
+    schedule_interval=timedelta(days=1)
+)
+
+task1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=pipeline
+)
