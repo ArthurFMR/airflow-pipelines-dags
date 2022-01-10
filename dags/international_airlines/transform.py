e89ac87ed2a2f4ba0cd3da3b7ccb10de12f906c7
Arthur <arthurcapacitacion@gmail.com>
2022-01-09 20:49:31 -0400

    Added Transform Step of international airlines

---
+++ dags/international_airlines/transform.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from airflow.decorators import task
+
+
+@task(task_id="transform")
+def transform(path:str):
+    dataframe = pd.read_csv(path)
+
+    columns_to_remove = ['Month', 'Freight_In_(tonnes)', 'Mail_In_(tonnes)', 'Freight_Out_(tonnes)',
+                        'Mail_Out_(tonnes)']
+
+    transformed_df = dataframe.drop(columns_to_remove, axis=1)
+    transformed_df.columns = map(str.lower, transformed_df.columns)
+
+    transformed_df.to_csv(path)
