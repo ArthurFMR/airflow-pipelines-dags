84438325439c702bab9bc4f3ff34da4111d831b3
Arthur <arthurcapacitacion@gmail.com>
2022-01-13 22:29:45 -0400

    Testing dag decorator on macdonald nutrients etl

--- dags/macdonald_nutrients/pipeline.py
+++ dags/macdonald_nutrients/pipeline.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from airflow.decorators import dag
 
 from datetime import datetime, timedelta
 
@@ -13,16 +14,24 @@ default_args = {
     'retry_delay': timedelta(minutes=1)
 }
 
-with DAG(
-    'macdonald_nutrients_pipeline', 
-    default_args=default_args, 
-    schedule_interval=timedelta(days=30),
-    catchup=False
-    ) as dag:
-
+@dag(schedule_interval=None, start_date=datetime.now() + timedelta(minutes=3), catchup=False)
+def macdonald_nutrients_etl():
     foods_list = fetch_all_food_data()
     transformed_foods_data_list = structure_all_foods_data(foods_list)
     load_data(transformed_foods_data_list)
+
+macdonald_nutrients_etl_dag = macdonald_nutrients_etl()
+
+# with DAG(
+#     'macdonald_nutrients_pipeline', 
+#     default_args=default_args, 
+#     schedule_interval=timedelta(days=30),
+#     catchup=False
+#     ) as dag:
+
+#     foods_list = fetch_all_food_data()
+#     transformed_foods_data_list = structure_all_foods_data(foods_list)
+#     load_data(transformed_foods_data_list)
 
 
 
